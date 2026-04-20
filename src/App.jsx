b0292703b45ad6750308f0a2da5012874670d5f3
Lyric Maselli <lyricmaselli@MacBook-Pro-83.local>
2026-04-19 22:59:44 -0500

    Add scroll-to-top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import ScrollToTop from './components/ScrollToTop'
 import Footer from './components/Footer'
 import FloatingActions from './components/FloatingActions'
 import Home from './pages/Home'
@@ -13,6 +14,7 @@ import HomeValuation from './pages/HomeValuation'
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
